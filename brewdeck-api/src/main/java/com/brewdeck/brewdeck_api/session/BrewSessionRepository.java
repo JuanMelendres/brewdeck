7d72c1c4d7e2edac8679581b0ce684a51e5e717b
Juan Melendres <juan.mele97@gmail.com>
2026-05-14 12:27:59 -0600

    feat(api): add basic filtering for brew

--- brewdeck-api/src/main/java/com/brewdeck/brewdeck_api/session/BrewSessionRepository.java
+++ brewdeck-api/src/main/java/com/brewdeck/brewdeck_api/session/BrewSessionRepository.java
@@ -2,7 +2,10 @@ package com.brewdeck.brewdeck_api.session;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface BrewSessionRepository extends JpaRepository<BrewSession, Long> {
+public interface BrewSessionRepository
+    extends JpaRepository<BrewSession, Long>, JpaSpecificationExecutor<BrewSession> {
+
   List<BrewSession> findByRecipeIdOrderByBrewedAtDesc(Long recipeId);
 }
